ebf317419593fe909c39fc2a0ea3cf67329fb81a
Davi C. Pes. <davicalo2d@gmail.com>
2025-07-09 12:45:27 -0300

    changer name

--- src/main/java/br/com/secret/friend/SecretFriendApplication.java
+++ src/main/java/br/com/secret/friend/SecretFriendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmigoSecretoApplication {
+public class SecretFriendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmigoSecretoApplication.class, args);
+		SpringApplication.run(SecretFriendApplication.class, args);
 	}
 
 }
