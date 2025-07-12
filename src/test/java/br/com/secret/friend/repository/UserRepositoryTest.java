721532609d71de3eb07d1f982e6691635df3fb97
Davi C. Pes. <davicalo2d@gmail.com>
2025-07-12 16:47:40 -0300

    springdoc

---
+++ src/test/java/br/com/secret/friend/repository/UserRepositoryTest.java
@@ -0,0 +1,18 @@
+package br.com.secret.friend.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+}
